d3318a4b47f6d8d750508d7e5718872e94e21ed3
Engels Tercero <higashikataengels@icloud.com>
2023-03-11 21:14:43 -0600

    first commit

---
+++ bin/vapid_algorithms.dart
@@ -0,0 +1,5 @@
+import 'package:vapid_algorithms/vapid_algorithms.dart' as vapid_algorithms;
+
+void main(List<String> arguments) {
+  print('Hello world: ${vapid_algorithms.calculate()}!');
+}
---
+++ test/vapid_algorithms_test.dart
@@ -0,0 +1,8 @@
+import 'package:vapid_algorithms/vapid_algorithms.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
